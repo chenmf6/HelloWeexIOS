979aeff39f75c4918d083a821b3c6ed431867f3c
chenminfei <chenminfei@xiao100.com>
2018-06-28 23:45:50 -0700

    Initial Commit

---
+++ HelloWeex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWeex
+//
+//  Created by chenmf on 2018/6/28.
+//  Copyright © 2018年 xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWeex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWeex
+//
+//  Created by chenmf on 2018/6/28.
+//  Copyright © 2018年 xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
